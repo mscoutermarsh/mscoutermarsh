b41db61a3eaf8db3f8eac2ba873b5f44e6528095
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2020-07-04 20:08:16 -0700

    fix html

--- html_css_to_image.rb
+++ html_css_to_image.rb
@@ -6,8 +6,9 @@ module HtmlCssToImage
 
   def self.url(html:, css: nil, google_fonts: nil, ms_delay: nil, selector: nil, viewport_height: nil, viewport_width: nil)
     image = HTTParty.post("https://hcti.io/v1/image",
-                          body: { html: html, css: css, google_fonts: google_fonts, ms_delay: ms_delay, selector: selector, viewport_height: viewport_height, viewport_width: viewport_width },
+                          body: { html: html, css: css, google_fonts: google_fonts, ms_delay: ms_delay, selector: selector },
                           basic_auth: AUTH)
+    puts image
 
     image["url"]
   end
--- tweet_to_image.rb
+++ tweet_to_image.rb
@@ -2,14 +2,12 @@ require_relative "html_css_to_image.rb"
 
 module TweetToImage
   def self.url(tweet_id)
-    html = `
-<blockquote class="twitter-tweet" style="width: 400px;" data-dnt="true">
-<p lang="en" dir="ltr"></p>
+    html = "<blockquote class='twitter-tweet' style='width: 400px;' data-dnt='true'>
+<p lang='en' dir='ltr'></p>
 
-<a href="https://twitter.com/fortnitegame/status/#{tweet_id}"></a>
+<a href='https://twitter.com/fortnitegame/status/#{tweet_id}'></a>
 
-</blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    `
+</blockquote> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>"
 
     HtmlCssToImage.url(html: html, ms_delay: 1500, selector: ".twitter-tweet")
   end
